0dc3890527dbf2363da651e08112663778575efc
LuisMFG <luismiguelfloresgr@gmail.com>
2021-11-24 23:35:12 -0300

    CREANDO COMPONENTE CON PROPS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 
 const text = ["Boton 1", "Boton 2", "Boton 3"]
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer greeting="Mi primer componente usando props"/>
     </div>
   );
 }
--- src/components/CarWidget/CarWidget.js
+++ src/components/CarWidget/CarWidget.js
@@ -3,10 +3,15 @@ import "./CarWidget.css"
 const CarWidget = () => {
     return (
         <div>
-
-            <li className="nav-item">
-            <img className="carWidget" src="https://icones.pro/wp-content/uploads/2021/05/symbole-charrette-verte.png"/>
-            </li>
+             <ul className="navbar-nav">    
+                <li className="nav-item">
+                <img className="carWidget" src="https://icones.pro/wp-content/uploads/2021/05/symbole-charrette-verte.png"/>
+                </li>
+                
+                <li className="nav-item">
+                <a className="nav-link numeroCarrito itemListContainer">10</a>
+                </li>
+            </ul>
 
         </div>
     )
