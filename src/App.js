700f4426cf0e65a3f741f6b254320cdafa5c6c0e
LuisMFG <luismiguelfloresgr@gmail.com>
2021-11-28 20:15:25 -0300

    AÑADIENDO LOGICA DE CONTADORES SIN CONDICIONAL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import './App.css';
+import { useState } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
-function App() {
+// setContador( contador > 10 ? contador + 1 : alert("No hay stock disponible") )
+
+// const useContador = () => {
+//   const [contador, setContador] = useState(0)
+  
+//   const incrementar = () => {
+//     setContador( contador + 1 )
+//   }
+  
+//   const disminuir = () => {
+//     setContador ( contador - 1)
+//   }
+  
+//   return( contador, incrementar, disminuir )
+  
+// }
+
+const App = () => {
+  // const { contador, incrementar, disminuir } = useContador()
+  
+  const [contador, setContador] = useState(0)
+  
+  const incrementar = () => {
+    setContador( contador + 1 )
+  }
+  
+  const disminuir = () => {
+    setContador ( contador - 1 )
+  }
+  
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting="Mi primer componente usando props"/>
+      <button onClick={incrementar}>+</button>
+      <button onClick={disminuir}>-</button>
+      <h1>{contador}</h1>
     </div>
   );
 }
