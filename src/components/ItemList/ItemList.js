c7ac9eb2c1400f202a6e356825be59858409e4f3
LuisMFG <luismiguelfloresgr@gmail.com>
2021-11-30 22:45:51 -0300

    CREANDO E IMPORTANDO ITEM Y ITEMLIST

---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,10 @@
+import "./ItemList.css"
+import Item from "../Item/Item"
+
+const ItemList = (items) => {
+    return (
+        <Item />
+    )
+}
+
+export default ItemList
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,13 +1,26 @@
 import "./ItemListContainer.css"
 import ItemCount from "../ItemCount/ItemCount"
+import { useEffect } from "react"
+import ItemList from "../ItemList/ItemList"
 
 const ItemListContainer = ({greeting}) => {
+
+    // useEffect(() => {
+    //     const list = productos ()
+
+    //     list.then((respuesta) => {
+
+    //     })
+
+    // }, [])
 
     return (
         <>
         <h3>{greeting}</h3>
         <ItemCount stock={10} inicial={1} onAdd="Cantidad de productos agregados al carrito:"/>
+        <ItemList />
         </>
+        // <ItemList />
     )
 }
 
